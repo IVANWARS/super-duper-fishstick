d0067fd2f3fff49c72c6f867418445c6fb216389
IVANWARS <IVANWARS230@yandex.ru>
2024-01-31 13:58:58 +0300

    добавил пример

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿int N = 453;
+int firstDigit = N / 100; // 453 / 100 = 4
+int thirdDigit = N % 10; // 453 % 10 = 3
+int result = firstDigit * 10 + thirdDigit; // 4 * 10 + 3 = 43
+
+Console.WriteLine($"В числе {N} => {result}");
